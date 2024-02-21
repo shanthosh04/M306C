6799a2239ab2c13ff9727a8da60384af676ae857
Dimitri9988 <dimitri.steiner@ict.csbe.ch>
2024-02-21 09:28:14 +0100

    Fehler identifizirt

--- server/entry.js
+++ server/entry.js
@@ -29,40 +29,40 @@ const add = async (req, res) => {
     birthDate,
     ahvNumber,
   } = req.body;
-  console.log(internshipContract, legalGuardian, intershipCompany, )
+
+  console.log(firstname,
+    lastname,
+    imageApplicant,
+    address,
+    cityAndZip,
+    country,
+    field,
+    classname,
+    qvYear,
+    certificate,
+    noteQV,
+    internshipContract,
+    efzCopy,
+    legalGuardian,
+    applicationDate,
+    intershipCompany,
+    responsiblePerson,
+    applicationStatus,
+    interviewDate,
+    trialVisitDate,
+    contractCreationDate,
+    internshipSalary1,
+    internshipSalary2,
+    mbaApprovalDate,
+    birthDate,
+    ahvNumber, )
+
+
   const userId = 1;
   console.log(userId);
   const query = `INSERT INTO entries(user_id, firstname, lastname, imageApplicant, address, cityAndZip, country, field, classname, qvYear, certificate, noteQV, internshipContract, efzCopy, legalGuardian, applicationDate, internshipCompany, responsiblePerson, applicationStatus, interviewDate, trialVisitDate, contractCreationDate, internshipSalary1, internshipSalary2, mbaApprovalDate, birthDate, ahvNumber) VALUES ( 1, "${firstname}", "${lastname}", "${imageApplicant}", "${address}", "${cityAndZip}", "${country}", "${field}", "${classname}", "${qvYear}", "${certificate}", "${noteQV}", "${internshipContract}", "${efzCopy}", "${legalGuardian}", "${applicationDate}", "${intershipCompany}", "${responsiblePerson}", "${applicationStatus}", "${interviewDate}", "${trialVisitDate}", "${contractCreationDate}", ${internshipSalary1}, ${internshipSalary2}, "${mbaApprovalDate}", "${birthDate}", "${ahvNumber}");`;
   try {
-    const result = await executeSQL(
-      query,
-      firstname,
-      lastname,
-      imageApplicant,
-      address,
-      cityAndZip,
-      country,
-      field,
-      classname,
-      qvYear,
-      certificate,
-      noteQV,
-      intershipContract,
-      efzCopy,
-      legalGuardian,
-      applicationDate,
-      intershipCompany,
-      responsiblePerson,
-      applicationStatus,
-      interviewDate,
-      trialVisitDate,
-      contractCreationDate,
-      internshipSalary1,
-      internshipSalary2,
-      mbaApprovalDate,
-      birthDate,
-      ahvNumber
-    );
+    const result = await executeSQL(query);
     if (result.affectedRows === 1) {
       res.json({ success: true });
     } else {
