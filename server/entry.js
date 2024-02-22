6ce4f09f3cf4c293ee7a5184d1b03198f4be120c
shanthosh04 <shanthosh1610@gmail.com>
2024-02-22 13:43:34 +0100

    add optional entry

--- server/entry.js
+++ server/entry.js
@@ -30,44 +30,17 @@ const add = async (req, res) => {
     ahvNumber,
   } = req.body;
 
-  console.log(firstname,
-    lastname,
-    imageApplicant,
-    address,
-    cityAndZip,
-    country,
-    field,
-    classname,
-    qvYear,
-    certificate,
-    noteQV,
-    internshipContract,
-    efzCopy,
-    legalGuardian,
-    applicationDate,
-    intershipCompany,
-    responsiblePerson,
-    applicationStatus,
-    interviewDate,
-    trialVisitDate,
-    contractCreationDate,
-    internshipSalary1,
-    internshipSalary2,
-    mbaApprovalDate,
-    birthDate,
-    ahvNumber, )
-
-    const values = [
-      1, firstname, lastname, imageApplicant, address, cityAndZip, country, field, classname,
-      qvYear, certificate, noteQV, internshipContract ? internshipContract : null, efzCopy, legalGuardian ? legalGuardian : null, applicationDate,
-      intershipCompany, responsiblePerson, applicationStatus, interviewDate, trialVisitDate,
-      contractCreationDate ? contractCreationDate : null,
-      internshipSalary1, internshipSalary2, mbaApprovalDate ? mbaApprovalDate : null, birthDate, ahvNumber
-    ];
+  const values = [
+    1, firstname, lastname, imageApplicant, address, cityAndZip, country, field, classname,
+    qvYear, certificate, noteQV, internshipContract || null, efzCopy, legalGuardian || null, applicationDate,
+    intershipCompany, responsiblePerson, applicationStatus, interviewDate, trialVisitDate,
+    contractCreationDate || null,
+    internshipSalary1, internshipSalary2, mbaApprovalDate || null, birthDate, ahvNumber
+  ];
 
   const userId = 1;
   console.log(userId);
-  const query = `INSERT INTO entries(user_id, firstname, lastname, imageApplicant, address, cityAndZip, country, field, classname, qvYear, certificate, noteQV, internshipContract, efzCopy, legalGuardian, applicationDate, internshipCompany, responsiblePerson, applicationStatus, interviewDate, trialVisitDate, contractCreationDate, internshipSalary1, internshipSalary2, mbaApprovalDate, birthDate, ahvNumber) VALUES ( 1, "${firstname}", "${lastname}", "${imageApplicant}", "${address}", "${cityAndZip}", "${country}", "${field}", "${classname}", "${qvYear}", "${certificate}", "${noteQV}", "${internshipContract}", "${efzCopy}", "${legalGuardian}", "${applicationDate}", "${intershipCompany}", "${responsiblePerson}", "${applicationStatus}", "${interviewDate}", "${trialVisitDate}", "${contractCreationDate}", ${internshipSalary1}, ${internshipSalary2}, "${mbaApprovalDate}", "${birthDate}", "${ahvNumber}");`;
+  const query = `INSERT INTO entries(user_id, firstname, lastname, imageApplicant, address, cityAndZip, country, field, classname, qvYear, certificate, noteQV, internshipContract, efzCopy, legalGuardian, applicationDate, internshipCompany, responsiblePerson, applicationStatus, interviewDate, trialVisitDate, contractCreationDate, internshipSalary1, internshipSalary2, mbaApprovalDate, birthDate, ahvNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   try {
     const result = await executeSQL(query, values);
     if (result.affectedRows === 1) {
@@ -92,12 +65,12 @@ const showAllEntries = async (req, res) => {
 }
 
 const getEntryById = async (req, res) => {
-  const entryId = req.params.id; // Die ID der Firma aus den Routenparametern abrufen
+  const entryId = req.params.id;
   const query = `SELECT * FROM entries WHERE id = ${entryId}`;
   try {
     const result = await executeSQL(query);
     if (result.length === 1) {
-      res.json(result[0]); // Nur die erste gefundene Firma zurückgeben (es sollte nur eine sein, da es sich um eine eindeutige ID handelt)
+      res.json(result[0]);
     } else {
       res.status(404).json({ error: "Eintrag nicht gefunden" });
     }
